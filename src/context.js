dd98f9ffe685a765b3858947a5525c535f9f9b0a
MahmoudHossameldin <mahmoud.ahmed.aw@gmail.com>
2022-03-15 20:16:31 +0200

    mapped the images to show in the homepage

--- src/context.js
+++ src/context.js
@@ -1,11 +1,30 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const Context = createContext();
 
 function ContextProvider({ children }) {
   const [allPhotos, setAllPhotos] = useState([]);
 
-  return <Context.Provider value={{ photos }}>{children}</Context.Provider>;
+  useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
+    )
+      .then((res) => res.json())
+      .then((data) => setAllPhotos(data));
+  }, []);
+
+  return <Context.Provider value={{ allPhotos }}>{children}</Context.Provider>;
 }
 
 export { Context, ContextProvider };
+
+//   useEffect(() => {
+//     async function getImages() {
+//       const res = await fetch(
+//         "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
+//       );
+//       const data = res.json();
+//       setAllPhotos(data);
+//     }
+//     getImages();
+//   }, []);
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
-import { ContextProvider } from "./context";
+import { ContextProvider } from "./Context";
 
 ReactDOM.render(
   <ContextProvider>
