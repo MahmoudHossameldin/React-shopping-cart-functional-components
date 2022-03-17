e1f36a7be4628915587e02c6e6294572cc931e64
MahmoudHossameldin <mahmoud.ahmed.aw@gmail.com>
2022-03-17 18:11:16 +0200

    added the functionality to add product to or remove product from favorites

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { Context } from "../Context";
 function Image({ className, img }) {
+  const [hovered, setHovered] = useState(false);
+  const { toggleFavorite } = useContext(Context);
+
+  const filledHeart = (
+    <i
+      className="ri-heart-fill favorite"
+      onClick={() => toggleFavorite(img.id)}
+    ></i>
+  );
+  const emptyHeart = (
+    <i
+      className="ri-heart-line favorite"
+      onClick={() => toggleFavorite(img.id)}
+    ></i>
+  );
+  const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>;
+
+  function heartIcon() {
+    if (img.isFavorite) return filledHeart;
+    else if (hovered) return emptyHeart;
+  }
+
   return (
-    <div className={`${className} image-container`}>
+    <div
+      className={`${className} image-container`}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
       <img src={img.url} className="image-grid" />
+      {heartIcon()}
+      {cartIcon}
     </div>
   );
 }
